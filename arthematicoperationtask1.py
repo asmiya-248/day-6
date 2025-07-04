43371f37a006e48e981f701350edfacd4d080061
unknown <asmiya.s248@gmail.com>
2025-07-04 14:21:42 +0530

    arthematictask1

---
+++ arthematicoperationtask1.py
@@ -0,0 +1,25 @@
+a=2505
+b=143
+c=20.5
+d=88.8
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+f=a+b+c+d #2757.3
+print("addition:" ,f)
+print(type(f))
+s=a-b-c
+print("subtraction:")
+print(type(s))
+m=a*d
+print("Multiplication:" ,m)
+print(type(m))
+d=c/d
+print("Division:" ,d)
+print(type(div))
+print("A+B+C+D" ,f)
+print(type(f))
+
+
---
+++ assign.py
@@ -0,0 +1,4 @@
+a=999
+print("A value is:",a)
+a+=10 #A=A+10
+Print("a+=10:" ,a)
